605bf8c7586b067c281420ae8522007403c1a913
Gaetan Wcz <gaetan.wichlacz@ext.adeo.com>
2025-06-14 12:51:27 +0000

    remove comment + fix commit from devcontainers

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,10 +1,8 @@
-// packages/server/src/db/index.ts
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema.js';
 
 const connectionString = process.env.DATABASE_URL || 'postgres://user:password@localhost:5432/mydb';
 
-// Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(connectionString, { prepare: false });
 export const db = drizzle(client, { schema });
